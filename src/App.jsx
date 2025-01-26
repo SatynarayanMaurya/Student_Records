5a36f45ee9d67f3668289ab346b9440f5e0e1eaf
Satynarayan Maurya <123947617+SatynarayanMaurya@users.noreply.github.com>
2025-01-27 00:36:06 +0530

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,29 @@
 import Homepage from "./Components/Homepage"
 import Navbar from "./Components/Navbar"
-import {Routes, Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import StudentDashboard from "./Components/StudentDashboard"
 import Logout from "./Components/Logout"
 import LoginPage from "./Components/LoginPage"
 import SignupPage from "./Components/SignupPage"
 import BasicHomepage from "./Components/BasicHomepage"
-function App() {
-  
-  const token = localStorage.getItem("token")
-<<<<<<< HEAD
-=======
 
->>>>>>> c1193b4ebee42f7d3b01247c7a6fc5a1225d12b7
+function App() {
+  const token = localStorage.getItem("token");
+
   return (
     <div>
-
-      <Navbar/>
-
+      <Navbar />
       <Routes>
-          <Route path="/login" element={<LoginPage/>}/>
-          <Route path="/signup" element={<SignupPage/>}/>
-<<<<<<< HEAD
-          <Route path="/" element={token ? <Homepage/> : <LoginPage/>}>
-            <Route path="/" element= {<BasicHomepage/>}/>
-            <Route path="/dashboard" element={token ? <StudentDashboard/> : <LoginPage/>}/>
-            <Route path="/logout" element={token ? <Logout/> : <LoginPage/>}/>
-=======
-          <Route path="/" element={token !== null ? <Homepage/> : <LoginPage/>}>
-            <Route path="/dashboard" element={token !== null? <StudentDashboard/> : <LoginPage/>}/>
-            <Route path="/logout" element={token!== null ? <Logout/> : <LoginPage/>}/>
->>>>>>> c1193b4ebee42f7d3b01247c7a6fc5a1225d12b7
-          </Route>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/" element={token !== null ? <Homepage /> : <LoginPage />}>
+          <Route path="/" element={<BasicHomepage />} />
+          <Route path="/dashboard" element={token !== null ? <StudentDashboard /> : <LoginPage />} />
+          <Route path="/logout" element={token !== null ? <Logout /> : <LoginPage />} />
+        </Route>
       </Routes>
-      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
